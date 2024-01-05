3d606b91816965caada8d3ed5e952183317fcfa1
reymanuel010899 <reymanuelferrera78@gmail.com>
2024-01-05 00:03:58 -0400

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## doctypes
+
+escuelas
+
+#### License
+
+MIT
---
+++ app_code_marina/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "doctypes",
+			"type": "module",
+			"label": _("doctypes")
+		}
+	]
